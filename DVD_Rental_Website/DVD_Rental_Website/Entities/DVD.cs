dbbe56d2719d1240498b749b39b78bd8f4f3392d
Janusika T. <UT01492@unicomtic.com>
2024-10-16 16:27:51 +0530

    DVDRepositoryUpdate

--- DVD_Rental_Website/DVD_Rental_Website/Entities/DVD.cs
+++ DVD_Rental_Website/DVD_Rental_Website/Entities/DVD.cs
@@ -2,6 +2,11 @@
 {
     public class DVD
     {
-        
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int CopiesAvailable { get; set; }
     }
 }
